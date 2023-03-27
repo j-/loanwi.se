b19f2a5a03df95610027a5eeeb3e0fb5ce55500a
j- <j@skeoh.com>
2023-03-28 00:23:03 +1100

    Remove links to other calculators

--- src/App.tsx
+++ src/App.tsx
@@ -48,24 +48,6 @@ const App: React.FC = () => {
           </div>
         </div>
       </form>
-
-      <br />
-
-      <p>Other calculators</p>
-
-      <ul className="list-disc pl-4">
-        <li><a href="https://www.calculator.net/repayment-calculator.html">Calculator.net Repayment Calculator</a></li>
-        <li><a href="https://www.calculator.net/loan-calculator.html">Calculator.net Loan Calculator</a></li>
-        <li><a href="https://www.calculator.net/payment-calculator.html">Calculator.net Payment Calculator</a></li>
-        <li><a href="https://www.loanmarket.com.au/calculators/loan-repayment-calculator">Loan Market</a></li>
-        <li><a href="https://www.commbank.com.au/digital/home-buying/calculator/home-loan-repayments">CommBank</a></li>
-        <li><a href="https://moneysmart.gov.au/budgeting/compound-interest-calculator">Moneysmart</a></li>
-        <li><a href="https://www.aussie.com.au/calculators/mortgage-repayments/">Aussie Home Loans</a></li>
-        <li><a href="https://www.walletwyse.com/calculators/home/">Walletwyse</a></li>
-        <li><a href="https://www.suncorp.com.au/banking/home-loan-repayment-calculator.html">Suncorp</a></li>
-        <li><a href="https://www.domain.com.au/home-loans/calculators/home-loan-repayment-calculator/">Domain</a></li>
-        <li><a href="https://mystate.com.au/business/loan-repayment-calculator/">MyState</a></li>
-      </ul>
     </div>
   );
 };
