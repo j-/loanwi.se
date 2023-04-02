b4a87a2c6f7664a21e2ef854a0b078d618d1bc28
j- <j@skeoh.com>
2023-04-02 15:19:51 +1000

    Add padding to currency field to align with header

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ const App: React.FC = () => {
           </a>
         </div>
         <div className="flex-1 flex flex-col justify-end">
-          <div>
+          <div className="p-4">
             <InputCurrency />
           </div>
         </div>
